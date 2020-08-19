f3f6ac5cace0660682b5906179a08eb4747e6045
yuxuan.liu <lyx@192.168.50.64>
2020-08-19 22:52:01 +0800

    week 04 结束

---
+++ week04/4/client.js
@@ -0,0 +1,253 @@
+const net = require('net')
+const parser = require('./parser.js')
+
+class ResponseParser {
+  constructor() {
+    // 挑战： 常量如何修改为函数？
+    this.WAITING_STATUS_LINE = 0
+    this.WAITING_STATUS_LINE_END = 1 // 因为以/r/n分割
+    this.WAITING_HEADER_NAME = 2
+    this.WAITING_HEADER_SPACE = 3 // 等待空格
+    this.WAITING_HEADER_VALUE = 4
+    this.WAITING_HEADER_LINE_END = 5
+    this.WAITING_HEADER_BLOCK_END = 6 // 等待空行
+    this.WAITING_BODY = 7
+
+    this.current = this.WAITING_STATUS_LINE
+    this.statusLine = ''
+    this.headers = {}
+    this.headerName = ''
+    this.headerValue = ''
+    this.bodyParser = null
+  }
+  get isFinished() {
+    return this.bodyParser && this.bodyParser.isFinished
+  }
+  get response() {
+    this.statusLine.match(/HTTP\/1.1 ([0-9]+) ([\s\S]+)/)
+    return {
+      statusCode: RegExp.$1,
+      statusText: RegExp.$2,
+      headers: this.headers,
+      body: this.bodyParser.content.join('')
+    }
+  }
+  receive(string) {
+    for (let i = 0; i < string.length; i++) {
+      this.receiveChar(string.charAt(i))
+    }
+  }
+
+  // HTTP/1.1 200 OK
+  // Content-Type: text/html
+  // Date: Sun, 16 Aug 2020 02:04:29 GMT
+  // Connection: keep-alive
+  // Transfer-Encoding: chunked
+  
+  // 2c1  // 16进制
+  // <html lang=en>
+  //           <head>
+  //             <title>Document</title>
+  //             <style>
+  //               body div #myid{
+  //                 width: 100px;
+  //                 background-color: #ff5000;
+  //               }
+  //               body div img{
+  //                 width: 30px;
+  //                 background-color: #ff1111;
+  //               }
+  //               .body.bodyDiv{
+  //                 width: 100px;
+  //                 background-color: #000;
+  //               }
+  //             </style>
+  //           </head>
+  //           <body>
+  //             <div>
+  //               <img />
+  //               <img id="myid" />
+  //             </div>
+  //             <div class="body bodyDiv">
+  //               111
+  //             </div>
+  //           </body>
+  //           </html>
+          
+  // 0
+  
+  receiveChar(char) {
+    if (this.current === this.WAITING_STATUS_LINE) {
+      if (char === '\r') {
+        this.current = this.WAITING_STATUS_LINE_END
+      } else {
+        this.statusLine += char
+      }
+    } else if (this.current === this.WAITING_STATUS_LINE_END) {
+      if (char === '\n') {
+        this.current = this.WAITING_HEADER_NAME
+      }
+    } else if (this.current === this.WAITING_HEADER_NAME) {
+      if (char === ':') {
+        this.current = this.WAITING_HEADER_SPACE
+      } else if (char === '\r') { // header结束
+        this.current = this.WAITING_HEADER_BLOCK_END
+        if (this.headers['Transfer-Encoding'] === 'chunked') { // 创建bodyParser
+          this.bodyParser = new ChunkedBodyParser()
+        }
+      } else {
+        this.headerName += char
+      }
+    } else if (this.current === this.WAITING_HEADER_SPACE) {
+      if (char === ' ') {
+        this.current = this.WAITING_HEADER_VALUE
+      }
+    } else if (this.current === this.WAITING_HEADER_VALUE) {
+      if (char === '\r') {
+        this.current = this.WAITING_HEADER_LINE_END
+        this.headers[this.headerName] = this.headerValue
+        this.headerName = ''
+        this.headerValue = ''
+      } else {
+        this.headerValue += char
+      }
+    } else if (this.current === this.WAITING_HEADER_LINE_END) {
+      if (char === '\n') {
+        this.current = this.WAITING_HEADER_NAME
+      }
+    } else if (this.current === this.WAITING_HEADER_BLOCK_END) {
+      if (char === '\n') {
+        this.current = this.WAITING_BODY
+      }
+    } else if (this.current === this.WAITING_BODY) {
+      this.bodyParser.receiveChar(char)
+    }
+  }
+}
+
+class Request {
+  constructor(options) {
+    this.method = options.method || 'GET'
+    this.host = options.host
+    this.port = options.port || 80
+    this.path = options.path || '/'
+    this.body = options.body || {}
+    this.headers = options.headers || {}
+
+    if (!this.headers['Content-Type']) {
+      this.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+    }
+
+    if (this.headers['Content-Type'] === 'application/json') {
+      this.bodyText = JSON.stringify(this.body)
+    } else if (this.headers['Content-Type'] === 'application/x-www-form-urlencoded') {
+      this.bodyText = Object.keys(this.body).map(key => `${key}=${encodeURIComponent(this.body[key])}`).join('&')
+    }
+
+    this.headers['Content-Length'] = this.bodyText.length
+  }
+
+  send(connection) {
+    return new Promise((resolve, reject) => {
+      const parser = new ResponseParser
+      if (connection) {
+        connection.write(this.toString())
+      } else {
+        connection = net.createConnection({
+          host: this.host,
+          port: this.port
+        }, () => {
+          connection.write(this.toString())
+        })
+      }
+
+      connection.on('data', (data) => {
+        console.log(data.toString())
+        parser.receive(data.toString())
+        if (parser.isFinished) {
+          resolve(parser.response)
+          connection.end()
+        }
+      })
+      connection.on('error', (err) => {
+        reject(err)
+        connection.end()
+      })
+    })
+  }
+
+  toString() {
+    return `${this.method} ${this.path} HTTP/1.1\r\n${Object.keys(this.headers).map(key => `${key}: ${this.headers[key]}`).join('\r\n')}\r\n\r\n${this.bodyText}`
+  }
+}
+
+class ChunkedBodyParser {
+  constructor() {
+    this.WAITING_LENGTH = 0
+    this.WAITING_LENGTH_LINE_END = 1
+    this.READING_TRUNK = 2
+    this.WAITING_NEW_LINE = 3
+    this.WAITING_NEW_LINE_END = 4
+    this.length = 0
+    this.content = []
+    this.isFinished = false
+    this.current = this.WAITING_LENGTH
+  }
+  receiveChar(char) {
+    if (this.current === this.WAITING_LENGTH) {
+      if (char === '\r') {
+        if (this.length === 0) {
+          this.isFinished = true
+        }
+        this.current = this.WAITING_LENGTH_LINE_END
+      } else {
+        this.length *= 16
+        this.length += parseInt(char, 16)
+      }
+    } else if (this.current === this.WAITING_LENGTH_LINE_END) {
+      if (char === '\n') {
+        this.current = this.READING_TRUNK
+      }
+    } else if (this.current === this.READING_TRUNK) {
+      if (!this.isFinished) {
+        this.content.push(char)
+        this.length--
+        if (this.length === 0) {
+          this.current = this.WAITING_NEW_LINE
+        }
+      }
+    } else if (this.current === this.WAITING_NEW_LINE) {
+      if (char === '\r') {
+        this.current = this.WAITING_NEW_LINE_END
+      }
+    } else if (this.current === this.WAITING_NEW_LINE_END) {
+      if (char === '\n') {
+        this.current = this.WAITING_LENGTH
+      }
+    }
+  }
+}
+
+void async function () {
+  let request = new Request({
+    method: 'POST',
+    host: '127.0.0.1',
+    port: 8088,
+    path: '/',
+    headers: {
+      ['X-Foo2']: 'customed'
+    },
+    body: {
+      name: 'rile',
+      age: 24
+    }
+  })
+
+  const response = await request.send()
+
+  // console.log(response, 'response')
+
+  let dom = parser.parseHTML(response.body)
+
+  console.log(dom)
+}()
--- week04/server.js
+++ week04/server.js
@@ -16,36 +16,36 @@ http.createServer((request, response) => {
     // response.end(' my name 12\n')
     response.end(
       `<html lang=en>
-          <head>
-            <title>Document</title>
-            <style>
-              body div #myid{
-                width: 100px;
-                background-color: #ff5000;
-              }
-              body div img{
-                width: 30px;
-                background-color: #ff1111;
-              }
-              .body.bodyDiv{
-                display: flex;
-                width: 100px;
-                background-color: #000;
-              }
-            </style>
-          </head>
-          <body>
-            <div>
-              <img />
-              <img id="myid" />
-            </div>
-            <div class="body bodyDiv">
-              <div>1111</div>
-              <div>2222</div>
-            </div>
-          </body>
-          </html>
-        `);
+        <head>
+          <title>Document</title>
+          <style>
+            #container{
+              display: flex;
+              width: 500px;
+              height: 300px;
+              background-color: rgb(255,255,255);
+            }
+            #container #flex1 {
+              width: 300px;
+              height: 100px;
+              background-color: rgb(255,0,0);
+            }
+            #container #flex2 {
+              flex: 1;
+              height: 300px;
+              background-color: rgb(0,255,0);
+            }
+          </style>
+        </head>
+        <body>
+          <div id="container">
+            <div id="flex1">1111</div>
+            <div id="flex2">2222</div>
+          </div>
+        </body>
+        </html>
+      `
+      );
   });
 }).listen(8088);
 
@@ -69,3 +69,44 @@ console.log('server started');
 //   </body>
 //   </html>
 // `
+
+
+// `<html lang=en>
+//   <head>
+//     <title>Document</title>
+//     <style>
+//       body div #myid{
+//         width: 100px;
+//         background-color: #ff5000;
+//       }
+//       body div img{
+//         width: 30px;
+//         background-color: #ff1111;
+//       }
+//       .body.bodyDiv{
+//         display: flex;
+//         width: 500px;
+//         height: 300px;
+//         background-color: #000;
+//       }
+//       #flex1 {
+//         width: 200px;
+//       }
+//       #flex2 {
+//         flex: 1;
+//       }
+//     </style>
+//   </head>
+//   <body>
+//     <div>
+//       <img />
+//       <img id="myid" />
+//     </div>
+//     <div class="body bodyDiv">
+//       <div id="flex1">1111</div>
+//       <div id="flex2">2222</div>
+//     </div>
+//   </body>
+//   </html>
+// `
+
